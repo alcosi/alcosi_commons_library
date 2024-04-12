340cec1495999d0ce8ba1bbfd8dc69041bc62c01
BreninSul <42633769+BreninSul@users.noreply.github.com>
2024-04-12 09:27:56 +0200

    Increase repetition of LARGE_TEXT and add initialization block
    The repetition count of LARGE_TEXT constant is increased in the SecuredConstants.kt test file. An initialization block is added to print out the length of the large text for debugging or verification purposes.

--- src/test/kotlin/com/alcosi/lib/secured/SecuredConstants.kt
+++ src/test/kotlin/com/alcosi/lib/secured/SecuredConstants.kt
@@ -37,9 +37,12 @@ object SecuredConstants {
         LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
         OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
         SOFTWARE.     
-        """.trimIndent().repeat(10000)
+        """.trimIndent().repeat(100000)
 
     val SMALL_TEXT = "Hello, World!"
     val AES_TEST_KEY = "997e3cb05773cad20f3087c0360044602ef6164745e6fc245b1962db04cece67"
     val EMPTY_TEXT = ""
+    init {
+        println("Large Text size "+LARGE_TEXT.length)
+    }
 }
