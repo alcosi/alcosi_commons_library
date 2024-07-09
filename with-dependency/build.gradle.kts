bb6ae16eb2e8bc08b7be37b23ca82a18051fc7da
BreninSul <42633769+BreninSul@users.noreply.github.com>
2024-07-09 21:24:23 +0200

    Update version numbers for dependencies and build
    The commit updates the version number for the project under build.gradle.kts. Alongside this, it increments the version numbers for the 'rest-template-logging-interceptor' and 'okhttp-logging-interceptor' dependencies.

--- with-dependency/build.gradle.kts
+++ with-dependency/build.gradle.kts
@@ -42,7 +42,7 @@ plugins {
 
 val appName = "commons-library-basic-dependency"
 val springVersion = "3.3.1"
-val depVersion = "4.0.16"
+val depVersion = "4.0.17"
 val kotlinCoroutinesVersion = "1.8.1"
 
 val jacksonVersion = "2.17.1"
@@ -141,8 +141,8 @@ dependencies {
     api("io.github.breninsul:java-timer-scheduler-starter:1.0.3")
     api("io.github.breninsul:synchronization-starter:1.0.2")
     api("io.github.breninsul:future-starter:1.0.2")
-    api("io.github.breninsul:rest-template-logging-interceptor:1.2.3")
-    api("io.github.breninsul:okhttp-logging-interceptor:1.1.3")
+    api("io.github.breninsul:rest-template-logging-interceptor:1.2.4")
+    api("io.github.breninsul:okhttp-logging-interceptor:1.1.4")
     api("org.apache.logging.log4j:log4j-api-kotlin:1.4.0")
     api("jakarta.servlet:jakarta.servlet-api:6.0.0")
     api("javax.annotation:javax.annotation-api:1.3.2")
