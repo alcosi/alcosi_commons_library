191cd11cb8a2615f9995cebb3a2f14d128ad596b
BreninSul <42633769+BreninSul@users.noreply.github.com>
2024-07-16 08:57:50 +0200

    Update project version and dependencies
    Project version was upgraded from 5.0.4 to 5.0.5 in build.gradle.kts. Correspondingly, the dependency version was also modified in the same file. Additionally, the version number of a specific dependency, servlet-logging-starter, was updated in the with-dependency build script.

--- with-dependency/build.gradle.kts
+++ with-dependency/build.gradle.kts
@@ -42,7 +42,7 @@ plugins {
 
 val appName = "commons-library-basic-dependency"
 val springVersion = "3.3.1"
-val depVersion = "5.0.4"
+val depVersion = "5.0.5"
 val kotlinCoroutinesVersion = "1.8.1"
 
 val jacksonVersion = "2.17.1"
@@ -143,7 +143,7 @@ dependencies {
     api("io.github.breninsul:future-starter:1.0.2")
     api("io.github.breninsul:rest-template-logging-interceptor:1.2.5")
     api("io.github.breninsul:okhttp-logging-interceptor:1.1.5")
-    api("io.github.breninsul:servlet-logging-starter:1.0.1")
+    api("io.github.breninsul:servlet-logging-starter:1.0.2")
     api("org.apache.logging.log4j:log4j-api-kotlin:1.4.0")
     api("jakarta.servlet:jakarta.servlet-api:6.0.0")
     api("javax.annotation:javax.annotation-api:1.3.2")
