8f9db784d57d44fad5bcd4d1486e3ce7f0957042
BreninSul <42633769+BreninSul@users.noreply.github.com>
2024-07-25 16:38:43 +0200

    Update Spring Boot to version 3.3.2
    Bump Spring Boot version from 3.3.1 to 3.3.2 in both `build.gradle.kts` files. This ensures compatibility with the latest Spring Boot features and fixes.

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ plugins {
     id("java-library")
     id("maven-publish")
     id("net.thebugmc.gradle.sonatype-central-portal-publisher") version "1.2.3"
-    id("org.springframework.boot") version "3.3.1"
+    id("org.springframework.boot") version "3.3.2"
     id("io.spring.dependency-management") version "1.1.5"
     id("org.jetbrains.kotlin.jvm") version kotlinVersion
     id("org.jetbrains.kotlin.plugin.spring") version kotlinVersion
--- with-dependency/build.gradle.kts
+++ with-dependency/build.gradle.kts
@@ -31,7 +31,7 @@ plugins {
     id("idea")
     id("java-library")
     id("maven-publish")
-    id("org.springframework.boot") version "3.3.1"
+    id("org.springframework.boot") version "3.3.2"
     id("io.spring.dependency-management") version "1.1.5"
     id("org.jetbrains.kotlin.jvm") version "2.0.0"
     id("org.jetbrains.kotlin.plugin.spring") version "2.0.0"
@@ -41,7 +41,7 @@ plugins {
 }
 
 val appName = "commons-library-basic-dependency"
-val springVersion = "3.3.1"
+val springVersion = "3.3.2"
 val depVersion = "5.0.5"
 val kotlinCoroutinesVersion = "1.8.1"
 
